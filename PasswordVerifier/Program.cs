02f029584de59a3beba1b52705c4ea87bb61ca92
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-08-18 14:17:02 -0400

    Initial commit

---
+++ PasswordVerifier/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace PasswordVerifier
+{
+    public class PasswordVerifier
+    {
+        public static bool Verify(string userInputPassword)
+        {
+            if (!ContainsNullOrWhiteSpace(userInputPassword) && IsMinLength(userInputPassword) && IsMinUppercase(userInputPassword) && IsMinLowercase(userInputPassword) && IsMinDigits(userInputPassword))
+            {
+                Console.WriteLine("\nYour password has been accepted.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nYour password is invalid because:");
+                if (ContainsNullOrWhiteSpace(userInputPassword)) Console.WriteLine("\t- Your password may not contain white space.");
+                if (!IsMinLength(userInputPassword)) Console.WriteLine("\t- Your password must be at least nine characters long.");
+                if (!IsMinUppercase(userInputPassword)) Console.WriteLine("\t- Your password must contain at least one uppercase letter.");
+                if (!IsMinLowercase(userInputPassword)) Console.WriteLine("\t- Your password must contain at least one lowercase letter.");
+                if (!IsMinDigits(userInputPassword)) Console.WriteLine("\t- Your password must contain at least one digit.");
+                Console.WriteLine("Please try again.");
+                return false;
+            }
+        }
+        public static bool ContainsNullOrWhiteSpace(string userInputPassword)
+        {
+            if (String.IsNullOrWhiteSpace(userInputPassword)) return true;
+            else
+            {
+                for (int i = 0; i < userInputPassword.Length; i++)
+                {
+                    if (char.IsWhiteSpace(userInputPassword, i)) return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsMinLength(string userInputPassword)
+        {
+            int minLength = 9;
+            if (userInputPassword.Length >= minLength) return true;
+            return false;
+        }
+        public static bool IsMinUppercase(string userInputPassword)
+        {
+            int minUppercase = 1, countOfUppercase = 0;
+            for (int i = 0; i < userInputPassword.Length; i++)
+            {
+                if (char.IsUpper(userInputPassword, i))
+                {
+                    countOfUppercase++;
+                    if (countOfUppercase == minUppercase) return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsMinLowercase(string userInputPassword)
+        {
+            int minLowercase = 1, countOfLowercase = 0;
+            for (int i = 0; i < userInputPassword.Length; i++)
+            {
+                if (char.IsLower(userInputPassword, i))
+                {
+                    countOfLowercase++;
+                    if (countOfLowercase == minLowercase) return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsMinDigits(string userInputPassword)
+        {
+            int minDigits = 1, countOfDigits = 0;
+            for (int i = 0; i < userInputPassword.Length; i++)
+            {
+                if (char.IsDigit(userInputPassword, i))
+                {
+                    countOfDigits++;
+                    if (countOfDigits == minDigits) return true;
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Password Verifier!");
+            bool tryAgain = true;
+            do
+            {
+                Console.WriteLine("\nYour password must contain the following:" +
+                    "\n\t- No white space characters" +
+                    "\n\t- At least 9 characters" +
+                    "\n\t- At least one uppercase letter" +
+                    "\n\t- At least one lowercase letter" +
+                    "\n\t- At least one digit");
+                Console.Write("\nPlease enter your password: ");
+                if (PasswordVerifier.Verify(Console.ReadLine())) tryAgain = false;
+            }
+            while (tryAgain);
+        }
+    }
+}
---
+++ PasswordVerifier_Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+using PasswordVerifier;
+
+namespace PasswordVerifier_Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData("", true)]
+        [InlineData(" ", true)]
+        [InlineData("\t", true)]
+        [InlineData("\n", true)]
+        [InlineData("string with\twhite\nspace", true)]
+        [InlineData("str", false)]
+        public void TestContainsNullOrWhiteSpaceTrue(string str, bool expected) => Assert.Equal(PasswordVerifier.PasswordVerifier.ContainsNullOrWhiteSpace(str), expected);
+
+        [Theory]
+        [InlineData("aStrOf_09", true)]
+        [InlineData("aStrOf__10", true)]
+        [InlineData("aStrof7", false)]
+        [InlineData("aStrOf_8", false)]
+        public void TestIsMinLengthTrue(string str, bool expected) => Assert.Equal(PasswordVerifier.PasswordVerifier.IsMinLength(str), expected);
+
+        [Theory]
+        [InlineData("oneUppercase", true)]
+        [InlineData("TwoUppercase", true)]
+        [InlineData("ABC123", true)]
+        [InlineData("123ABC", true)]
+        [InlineData("lowercase", false)]
+        [InlineData("abc123", false)]
+        [InlineData("123abc", false)]
+        public void TestIsMinUppercaseTrue(string str, bool expected) => Assert.Equal(PasswordVerifier.PasswordVerifier.IsMinUppercase(str), expected);
+
+        [Theory]
+        [InlineData("oNELOWERCASE", true)]
+        [InlineData("tWOlOWERCASE", true)]
+        [InlineData("abc123", true)]
+        [InlineData("123abc", true)]
+        public void TestIsMinLowercaseTrue(string str, bool expected) => Assert.Equal(PasswordVerifier.PasswordVerifier.IsMinLowercase(str), expected);
+
+        [Theory]
+        [InlineData("digits_1", true)]
+        [InlineData("digits_12", true)]
+        [InlineData("noDigits", false)]
+        [InlineData("also_noDigits", false)]
+        public void TestIsMinDigitsTrue(string str, bool expected) => Assert.Equal(PasswordVerifier.PasswordVerifier.IsMinDigits(str), expected);
+    }
+}
